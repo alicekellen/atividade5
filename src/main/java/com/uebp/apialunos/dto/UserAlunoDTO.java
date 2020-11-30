afdf67a4315206dce7dd63fb4f4a15c75df1042d
Tamyres <tamyrescarla@gmail.com>
2020-11-29 21:46:45 -0300

    correções

--- src/main/java/com/uebp/apialunos/dto/UserAlunoDTO.java
+++ src/main/java/com/uebp/apialunos/dto/UserAlunoDTO.java
@@ -1,20 +1,20 @@
 package com.uebp.apialunos.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
-
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString
-@EqualsAndHashCode
 public class UserAlunoDTO {
-private String email;
-private String username;
-private String password;
+	private String email;
+	private String username;
+	private String password;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
 }
--- src/main/java/com/uebp/apialunos/models/UserAluno.java
+++ src/main/java/com/uebp/apialunos/models/UserAluno.java
@@ -7,34 +7,41 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
-
-@Getter
-@Setter
-@ToString
-@NoArgsConstructor
 @Entity(name = "user_aluno")
 @Table(name = "user_aluno")
-public class UserAluno  {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    
-    @Column(unique = true)
-    private String email;
+public class UserAluno {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @Column(unique = true)
-    private String username;
+	@Column(unique = true)
+	private String email;
 
-    private String password;
+	@Column(unique = true)
+	private String username;
 
-    public UserAluno(String email, String username, String password) {
-        this.username = username;
-        this.email = email;
-        this.password = password;
-    }
+	private String password;
+
+	public UserAluno(String email, String username, String password) {
+		this.username = username;
+		this.email = email;
+		this.password = password;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
 
 }
